06de335f5a22e587c598d0c5a2a52234e74210b7
Nathan <99520642+E30895@users.noreply.github.com>
2024-06-14 07:56:18 -0300

    Update one.py

--- one.py
+++ one.py
@@ -1,22 +1,29 @@
-pip install pandas openpyxl
 import pandas as pd
 
-import pandas as pd
+pd.set_option('display.float_format', '{:.2f}'.format)
 
-# Carregar as planilhas
-df_projecoes = pd.read_excel('projecoes.xlsx')
-df_observados = pd.read_excel('observados.xlsx')
+df_observados = pd.read_excel('1_Dados_202404.xlsx')
+df_projecoes = pd.read_excel('1_Projecoes_tesouraria.xlsx')
 
-# Mesclar as duas planilhas com base na coluna 'Data' e 'Indicador'
-df_merged = pd.merge(df_projecoes, df_observados, on=['Data', 'Indicador'], how='left', suffixes=('_proj', '_obs'))
+df_merged = pd.merge(df_projecoes, df_observados, on=['anomes','ag'], how='left', suffixes=('_proj', '_obs'))
 
-# Substituir os valores projetados pelos observados onde houver correspondência
-df_merged['Valor'] = df_merged['Valor_obs'].combine_first(df_merged['Valor_proj'])
+final = pd.DataFrame()
+final[['anomes', 'ag', 'Ano', 'Mês', 'COOP']] = df_merged[['anomes', 'ag', 'Ano', 'Mês', 'COOP']]
 
-# Selecionar apenas as colunas necessárias para o resultado final
-df_resultado = df_merged[['Data', 'Indicador', 'Valor']]
+final['rec_op'] = df_merged['rec_op_obs'].combine_first(df_merged['rec_op_proj'])
+final['des_op'] = df_merged['des_op_obs'].combine_first(df_merged['des_op_proj'])
+final['sob_liq'] = df_merged['sob_liq_obs'].combine_first(df_merged['sob_liq_proj'])
+final['pr_liq'] = df_merged['pr_liq_obs'].combine_first(df_merged['pr_liq_proj'])
+final['ind_liq'] = df_merged['ind_liq_obs'].combine_first(df_merged['ind_liq_proj'])
+final['mg_rwa'] = df_merged['mg_rwa_obs'].combine_first(df_merged['mg_rwa_proj'])
+final['roe'] = df_merged['roe_obs'].combine_first(df_merged['roe_proj'])
 
-# Salvar o resultado em uma nova planilha
-df_resultado.to_excel('resultado.xlsx', index=False)
+final['semestre'] = final['anomes'].map(lambda x:str(x).replace("2024", ""))
+final['semestre'] = pd.to_numeric(final['semestre'])
+final['semestre'] = ((final['semestre'] - 1) //6) + 1
+#final['semestre'] = final['semestre'].map(lambda x:str(x))
 
-print("Substituição concluída e resultado salvo em 'resultado.xlsx'")
+final['rec_op'] = final.groupby(['semestre', 'ag'])['rec_op'].cumsum()
+final['des_op'] = final.groupby(['semestre', 'ag'])['des_op'].cumsum()
+
+final.to_excel('9_final_teste.xlsx', index=False)
