d06d3916267313b9d634a4cae9e70043dd2c14e3
Nathan <99520642+E30895@users.noreply.github.com>
2024-07-12 11:53:00 -0300

    Update R.R

--- R.R
+++ R.R
@@ -5,3 +5,24 @@ df <- df %>%
       substr(as.character(ano_mes), 5, 6) %in% c("07", "08", "09", "10", "11", "12") ~ paste0(substr(as.character(ano_mes), 1, 4), "-S2")
     )
   )
+
+# Calculando a soma acumulada para os dados projetados
+df_projetado_acumulado <- df_projetado %>%
+  arrange(ag, ano_mes) %>%
+  group_by(ag, semestre) %>%
+  mutate(acumulado_projetado = cumsum(valor_projetado)) %>%
+  ungroup()
+
+# Mesclando com dados realizados
+df_combined <- df_projetado_acumulado %>%
+  left_join(df_realizado, by = c("ano_mes", "ag", "semestre")) %>%
+  mutate(
+    acumulado_realizado = ifelse(is.na(valor_realizado), NA, valor_realizado),
+    acumulado_final = ifelse(is.na(acumulado_realizado),
+                             acumulado_projetado + lag(acumulado_final, default = 0),
+                             acumulado_realizado)
+  ) %>%
+  group_by(ag, semestre) %>%
+  mutate(acumulado_final = cumsum(acumulado_final)) %>%
+  ungroup()
+
